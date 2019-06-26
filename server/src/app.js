b79936d219422b13366864f4ba99367fc9f0f269
echo2net <zhang2006@gmail.com>
2019-06-26 10:49:29 -0400

    without eslint

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,19 @@
-console.log('server')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status',(req,res)=>{
+    res.send({
+        message: 'hello world!'
+    })
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, ()=> console.log(`server started on port ${port}`));
